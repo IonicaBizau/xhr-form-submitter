71ce6d7499a33a31db7d71a1e8045e826efd18bc
Ionică Bizău <bizauionica@yahoo.com>
2014-06-11 10:57:03 +0300

    Create a new XHRFormSubmitter and use submit method

--- test/public/js/main.js
+++ test/public/js/main.js
@@ -2,9 +2,12 @@ window.onload = function () {
     var formEl = document.getElementById("myForm")
       , checkboxEl = document.getElementsByName("xhr")[0]
       , nameEl = document.getElementsByTagName("name")[0]
+      , formToSubmit = new XHRFormSubmitter({form: formEl})
       ;
 
-    formEl.addEventListener("submit", function () {
+    formEl.addEventListener("submit", function (e) {
         if (!checkboxEl.checked) { return; }
+        formToSubmit.submit();
+        e.preventDefault();
     });
 };
