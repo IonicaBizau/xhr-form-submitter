7c1cd8f73a2ce82c17c8f69d715871c5b65b8124
Ionică Bizău <bizauionica@yahoo.com>
2014-06-11 09:00:03 +0300

    Added submit method

--- src/xhr-form-submitter.js
+++ src/xhr-form-submitter.js
@@ -1,4 +1,41 @@
 (function (window) {
-    // te magic
+
+    var XHRFormSubmitter = function (options) {
+        var self = this;
+        self._form = options.form;
+
+        /**
+         * submit
+         * Submit the form using a XMLHttpRequest.
+         *
+         * @name submit
+         * @function
+         * @param {Object} submitOps The submit options
+         * @return {XMLHttpRequest} The XMLHttpRequest instance
+         */
+        self.submit = function (submitOps, callback) {
+            var xhr = new XMLHttpRequest()
+              , form = self._form
+              , url = submitOps.actionUrl || form.getAttribute("action")
+              , method = submitOps.method || form.getAttribute("method")
+              ;
+
+            callback = callback || function () {};
+
+            xhr.open(method, url);
+            xhr.setRequestHeader("content-type", "text/plain; charset=utf-8");
+            xhr.onreadystatechange = function() {
+                if (link.readyState !== 4) { return xhr; }
+
+                if (link.status >= 400) {
+                    return callback(link.responseText, null);
+                }
+
+                callback(null, link.responseText);
+            };
+
+            xhr.send(data);
+        };
+    };
     window.XHRFormSubmitter = XHRFormSubmitter;
 })(window);
