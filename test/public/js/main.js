478cb111157c1a69116607f9607491cc01ae55b3
Ionică Bizău <bizauionica@yahoo.com>
2014-06-11 11:37:57 +0300

    Added message boxes with CSS3 animations

--- test/public/js/main.js
+++ test/public/js/main.js
@@ -2,13 +2,27 @@ window.onload = function () {
     var formEl = document.getElementById("myForm")
       , checkboxEl = document.getElementsByName("xhr")[0]
       , nameEl = document.getElementsByTagName("name")[0]
+      , mbErrorEl = document.getElementById("mb-error")
+      , mbSuccessEl = document.getElementById("mb-success")
       , formToSubmit = new XHRFormSubmitter({form: formEl})
       ;
 
     formEl.addEventListener("submit", function (e) {
         if (!checkboxEl.checked) { return; }
         try {
-            formToSubmit.submit();
+            mbErrorEl.style.display = "none";
+            mbSuccessEl.style.display = "none";
+            formToSubmit.submit(function (err, data) {
+
+                var mb = null;
+                if (err) { mb = mbErrorEl; }
+                else { mb = mbSuccessEl; }
+
+                mb.innerHTML = err || data;
+                mb.style.display = "block";
+                mb.classList.remove("fadeInUp");
+                mb.classList.add("fadeInUp");
+            });
         } catch (e) {
             console.error(e.message);
         }
